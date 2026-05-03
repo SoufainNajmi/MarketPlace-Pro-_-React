aafcff02f0f18265c1b0cbd310dad13c5ccd9302
SoufainNajmi <soufainnajmi@gmail.com>
2026-05-04 01:11:17 +0200

    oranisation

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+export const initialMockProducts = [
+  { id: 1, name: 'Produit A Premium', price: 150 },
+  { id: 2, name: 'Produit B Essentiel', price: 300 },
+  { id: 3, name: 'Produit C Basique', price: 50 },
+  { id: 4, name: 'Produit D Pro', price: 120 },
+];
+
+export const initialMockSuppliers = [
+  { id: 1, name: 'Solutions Alpha' },
+  { id: 2, name: 'Distribution Beta' },
+  { id: 3, name: 'Fournisseur Global' },
+];
